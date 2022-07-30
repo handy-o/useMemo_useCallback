ffb0b7c0bdf9ee36932bc962dc3d7e5ad32f92f0
handy-o <dyhan9420@gmail.com>
2022-07-30 10:07:16 +0900

    updateCounter에 오타 수정

--- src/App.js
+++ src/App.js
@@ -12,16 +12,21 @@ import { useState, useCallback } from 'react';
 	이때 useCallback을 이용해서 props로 전달되는 함수자체를 useCallback의 첫번째 인수로 전달, 두번째 인수로 의존성 배열 등록
 	- props로 전달되는 인수인 함수자체를 메모이제이션 처리
 */
+/* 
+	useCallback에 의존성 배열을 등록하는 이유
+	- useCallback의 의존성 배열을 비워놓으면 함수에 활용되는 초기 state값 까지 메모이제이션되므로 매번 같은 값만 반환
+	- useCallback에 등록된 함수의 내부에 쓰이는 state값을 의존성 배열에 등록하면 해당 state값이 변경될 때만 메모이제이션 해제  
+*/
 
 function App() {
 	console.log('parent');
 	const [Counter, setCounter] = useState(0); //원시형
 	const [Input, setInput] = useState('');
-	const updateCounter = useCallback(() => setCounter(Counter + 1), []);
+	const updateCounter = useCallback(() => setCounter(Counter + 1), [Counter]);
 
 	// 자식 컴포넌트에 영향을 미치치 않는 부모컴포넌트에만 있는 state값이 변경돼서 재랜더링시
 	// memo로 감싸져 있는 자식 컴포넌트는 불필요하게 재런더링 되지 않음
-	const arr = [1, 2, 3]; //참조형
+
 	return (
 		<div className='app'>
 			<h1>Parent : {Counter}</h1>
@@ -35,7 +40,7 @@ function App() {
 
 			{/* 자식컴포넌트에 prop으로 전달되는 값이 원시형자료면 재랜더링방지 가능 */}
 			{/* 만약, 참조형자료가 전달되면 재랜더링 방지 안됨*/}
-			<Child Counter={Counter} arr={arr} updateCunter={updateCounter} />
+			<Child Counter={Counter} updateCounter={updateCounter} />
 			{/*  참조형 배열인 arr을 참조하면 child까지 재랜더링이 일어남 */}
 		</div>
 	);
