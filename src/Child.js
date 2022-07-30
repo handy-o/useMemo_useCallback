08f669956551f57795ae831fcee26571e7dffdb0
handy-o <dyhan9420@gmail.com>
2022-07-30 10:40:07 +0900

    주석설명 추가

--- src/Child.js
+++ src/Child.js
@@ -7,6 +7,26 @@ import { isEqual } from 'lodash'; //참조형 자료 값 비교
   렌더링된 결과물을 재활용(자식컴포넌트의 불필요한 재랜더링 방지)
 
   컴포넌트함수를 memo로 메모리제이션 처리 했더라도 부모로부터 전달받는 prop값이 있을 시 재호출됨
+*/
+
+/*
+  1. 부모 컴포넌트가 재랜더링시 자식 컴포넌트로 같이 재랜더링됨
+  2. 부모에서 자식으로 props전달 시 재랜더링됨
+
+  memo: 컴포넌트 자체를 memoization처리해서 재랜더링 방지
+  예외사항 : 배열, 객체, 함수같은 참조형 자료가 props로 전달될 때 재랜더링 됨
+
+  lodash - isEqual: props로 젇날되는 배열, 객체의 참조주소가 아닌 값자체를 비교해서 재랜더링 방지
+  useCallback: props로 전달되는 함수자체를 memoization
+  useMemo: 컴포넌트가 재랜더링시 매번 동일값을 리턴하면 해당 값 자체를 memoization
+
+
+  실행속도를 위해 메모리를 더 많이 할당
+  memoization을 불필요하게 너무 많이 적용하면 코드 가독성이 떨어짐
+  memoization된 요소는 가비지컬랙션에서 제외
+
+  garbage collection이란?
+  - 더이상 쓰이지 않는 메모리를 자동으로 제거
 
 */
 
